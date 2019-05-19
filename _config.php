29db4ebe47f6859220151bf9ec5b61f5f895463e
rob <rob@1stdesign.net>
2019-05-18 17:03:02 -0700

    updates

--- _config.php
+++ _config.php
@@ -4,17 +4,23 @@ use SilverStripe\Core\Config\Config;
 use SilverStripe\Core\Manifest\ModuleResourceLoader;
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use Silverstripe\Shortcodable\Shortcodable;
+use SilverStripe\View\Requirements;
+
+//Requirements::css( 'mi32dogs/silverstripe-shortcodable: css/shortcodable.css' );
+//Requirements::javascript( 'mi32dogs/silverstripe-shortcodable: javascript/editor_plugin.js' );
+//Requirements::javascript( 'mi32dogs/silverstripe-shortcodable: javascript/shortcodable.js' );
 
 // enable shortcodable buttons and add to HtmlEditorConfig
 $htmlEditorNames = Config::inst()->get(Shortcodable::class, 'htmleditor_names');
 
 if (is_array($htmlEditorNames)) {
     $plugin = ModuleResourceLoader::singleton()
-        ->resolveURL('mi32dogs/silverstripe-shortcodable:javascript/editor_plugin.js');
+        ->resolveURL('mi32dogs/silverstripe-shortcodable: javascript/editor_plugin.js');
 
     foreach ($htmlEditorNames as $htmlEditorName) {
         TinyMCEConfig::get($htmlEditorName)->enablePlugins(['shortcodable' => $plugin]);
-        TinyMCEConfig::get($htmlEditorName)->addButtonsToLine(1, 'shortcodable');
+        //TinyMCEConfig::get($htmlEditorName)->addButtonsToLine(1, '| shortcodable');
+        TinyMCEConfig::get($htmlEditorName)->insertButtonsAfter( 'code', ' | shortcodable' );
     }
 }
 
